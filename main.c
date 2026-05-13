f104ebb9a9aa316a93e64b2889b8a059fe789ab3
Vicente Lourenco Prista Von Bonhorst <vlourenc@c1r1s4.42lisboa.com>
2026-05-13 11:14:29 +0100

    done

--- main.c
+++ main.c
@@ -1,33 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vlourenc <vlourenc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/05/12 18:21:49 by vlourenc          #+#    #+#             */
-/*   Updated: 2026/05/13 10:10:41 by vlourenc         ###   ########.fr       */
+/*   Created: 2026/05/13 11:14:17 by vlourenc          #+#    #+#             */
+/*   Updated: 2026/05/13 11:14:18 by vlourenc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
-
-# include <stdlib.h>
-# include <stdbool.h>
-# include <limits.h> 
-
-typedef struct s_node
-{
-	int				value;
-	int				currpos;
-	int				pushcost;
-	bool			above_median;
-	bool			cheapest;
-
-	struct s_node	*target_node;
-	struct s_node	*next;
-	struct s_node	*prev;
-}	t_stack_node;
-
-#endif
---
+++ stack_init.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_init.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vlourenc <vlourenc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/12 18:50:46 by vlourenc          #+#    #+#             */
+/*   Updated: 2026/05/13 10:55:54 by vlourenc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	append_node(t_stack_node **stack, int n)
+{
+	t_stack_node	*node;
+	t_stack_node	*last_node;
+
+	if (!stack)
+		return ;
+	node = malloc(sizeof(t_stack_node));
+	if (!node)
+		return ;
+	node->next = NULL;
+	node->value = n;
+	node->cheapest = false;
+	if (!(*stack))
+	{
+		*stack = node;
+		node->prev = NULL;
+	}
+	else
+	{
+		last_node = find_last(*stack);
+		last_node->next = node;
+		node->prev = node;
+	}
+}
+
+t_stack_node	*find_last(t_stack_node *stack)
+{
+	if (!stack)
+		return (NULL);
+	while (stack->next)
+		stack = stack->next;
+	return (stack);
+}
